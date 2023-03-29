0ed0bf087b51cca23d6cc9b6f3e1f39d55504113
jadilovic <j.adilovic@gmail.com>
2023-03-29 21:20:32 +0200

    first section completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import reactLogo from './icons/react-logo.svg';
 import nodeLogo from './icons/nodejs-logo.svg';
 import githubLogo from './icons/github-tile.svg';
+import frontendMentor from './icons/frontendmentor.svg';
+import hackerRank from './icons/hackerrank.svg';
+import linkedin from './icons/linkedin-tile.svg';
 import developer from './images/developer.jpg';
 import './App.css';
 
@@ -10,25 +13,61 @@ function App() {
 			<header className="App-header">
 				<nav>
 					<div className="logos">
-						<img src={reactLogo} className="react-logo" alt="react logo" />
 						<img src={nodeLogo} className="node-logo" alt="node.js logo" />
+						<img src={reactLogo} className="react-logo" alt="react logo" />
 						<span className="App-link">Jasmin Adilovic</span>
 					</div>
-					<div>
-						<ul className="nav-links">
-							<li>
-								<a href="https://github.com/jadilovic">
-									<img
-										className="profile-link"
-										src={githubLogo}
-										alt="ja github profile"
-									/>
-								</a>
-							</li>
-						</ul>
-					</div>
+					<ul className="nav-links">
+						<li>
+							<a href="https://github.com/jadilovic">
+								<img
+									className="profile-link"
+									src={githubLogo}
+									alt="ja github profile"
+								/>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.frontendmentor.io/profile/jadilovic">
+								<img
+									className="profile-link"
+									src={frontendMentor}
+									alt="ja frontend-mentor profile"
+								/>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/in/jasmin-adilovic-2561aa17/">
+								<img
+									className="profile-link"
+									src={linkedin}
+									alt="ja linkedin profile"
+								/>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.hackerrank.com/jadilovic">
+								<img
+									className="profile-link"
+									src={hackerRank}
+									alt="ja hacker-rank profile"
+								/>
+							</a>
+						</li>
+					</ul>
 				</nav>
 				<div className="description">
+					<div className="welcome">
+						<h2>Nice to meet you!</h2>
+						<h4>Welcome to my portfolio site!</h4>
+						<h6>
+							Based in Bosnia, I'm <span className="react">React</span> and
+							<span className="node"> Node </span>
+							developer passionate about building progressive web apps that
+							users enjoy.
+						</h6>
+						<p>Contact me</p>
+					</div>
 					<img className="developer-img" src={developer} alt="" />
 				</div>
 			</header>
